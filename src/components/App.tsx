77798fc756c06925396aa1481db826517039798e
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-04-07 21:22:22 +1200

    feat: added base code for website header

---
+++ src/components/App.tsx
@@ -0,0 +1,53 @@
+import React, { ReactElement } from "react";
+import "../style/app.scss";
+
+function App(): ReactElement {
+	return (
+		<div className="App">
+			<header>
+				<section className="projects">
+					<ul>
+						<li>Breakout</li>
+						<li>Node REST API</li>
+						<li>Pacman</li>
+						<li>React CRUD</li>
+						<li>Upcoming</li>
+					</ul>
+				</section>
+				<section className="heading">
+					<h1>Aardhyn Lavender</h1>
+					<nav>
+						<button>Projects</button>
+						<button>About</button>
+						<button>Contact</button>
+					</nav>
+				</section>
+				<section className="details">
+					<ul>
+						<li>
+							<a
+								href="https://github.com/AardhynLavender/AardhynLavender"
+								target="_blank"
+							>
+								View Source
+							</a>
+						</li>
+						<li>
+							<p>BIT</p>
+							<span id="progression"></span>
+						</li>
+						<li>
+							<a href="https://unsplash.com/@godfrey15?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+								Photo Credit
+							</a>
+						</li>
+					</ul>
+				</section>
+			</header>
+			<main></main>
+			<footer></footer>
+		</div>
+	);
+}
+
+export default App;
