91f6097e15c6fae95e9cf74834b5f15e11dd1e03
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-07-30 15:47:17 +1200

    fix (public): correct page title next does not accept <title> tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "../style/app.scss";
+import Head from "next/head";
 
-const application = ({ Component, pageProps }) => <Component {...pageProps} />;
+const application = ({ Component, pageProps }) => (
+	<>
+		<Head>
+			<title>Aardhyn Lavender</title>
+		</Head>
+		<Component {...pageProps} />
+	</>
+);
 
 export default application;
